9e9d1725ca74c9228a4d5072e28a76cc16e77d0b
Daniel Possemato <possematod8884@my.sduhsd.net>
2019-05-28 09:50:49 -0700

    update name

---
+++ cdemo/areaofacircle2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float areaOfCircle(float r){
+   return r*r*3.14;
+ }
+
+void main() {
+  char input[256];
+  int r;
+
+ printf("input a radius\n");
+
+ while(1){
+  fgets(input, 256, stdin);
+  if(sscanf(input, "%d", &r) == 1) break;
+  printf("Invalid input\n");
+
+  }
+
+  printf("%f\n", areaOfCircle(r));
+
+
+//  for(float i=3.5; i<13; i++){
+  // printf("%f\n", areaOfCircle9i00;
+  //}
+}
